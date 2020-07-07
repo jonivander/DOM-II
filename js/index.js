3775026549c1f42a525a84ae324fc59ad2b02a3b
jonathan warner <j.m.warners@gmail.com>
2020-07-07 15:24:49 -0700

    Completed 2/10

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 // Your code goes here
 
 // mouseover
+const navLink = document.querySelectorAll('.nav-link')
+
+for (const link of navLink){
+    link.addEventListener('mouseover', function(event){
+        event.target.style.color = "blue"; 
+    })
+}
 
 // keydown
+const backGround = document.querySelector('body')
+
+backGround.addEventListener('keydown', function(event){
+    event.target.style.color = "skyblue";
+})
 
 // wheel
 
@@ -18,5 +30,5 @@
 
 // select
 
-//dblclick
+// dblclick
 
